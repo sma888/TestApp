aac319394c5e56139f3c7004d9aed9c0dc0f1168
sma888 <magamet.sitimov@gmail.com>
2022-02-06 20:49:54 +0300

    изменен цвет

--- GitHub/Form1.Designer.cs
+++ GitHub/Form1.Designer.cs
@@ -66,6 +66,7 @@ namespace GitHub
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
